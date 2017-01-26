9d655b79215b6ccd22ce4b387e27f27aa865d0c9
Yancey <yancej@amazon.com>
2017-01-26 14:17:29 -0800

    Adding in CustomAuthorizer classes and tests.

---
+++ Libraries/src/Amazon.Lambda.APIGatewayEvents/APIGatewayCustomAuthorizerRequest.cs
@@ -0,0 +1,23 @@
+﻿namespace Amazon.Lambda.APIGatewayEvents
+{
+    /// <summary>
+    /// For requests coming in to a custom API Gateway authorizer function.
+    /// </summary>
+    public class APIGatewayCustomAuthorizerRequest
+    {
+        /// <summary>
+        /// Gets or sets the 'type' property.
+        /// </summary>
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Gets or sets the 'authorizationToken' property.
+        /// </summary>
+        public string AuthorizationToken { get; set; }
+
+        /// <summary>
+        /// Gets or sets the 'methodArn' property.
+        /// </summary>
+        public string MethodArn { get; set; }
+    }
+}
---
+++ Libraries/src/Amazon.Lambda.APIGatewayEvents/APIGatewayCustomAuthorizerResponse.cs
@@ -0,0 +1,29 @@
+﻿namespace Amazon.Lambda.APIGatewayEvents
+{
+    using System.Runtime.Serialization;
+
+    /// <summary>
+    /// An object representing the expected format of an API Gateway authorization response.
+    /// </summary>
+    [DataContract]
+    public class APIGatewayCustomAuthorizerResponse
+    {
+        /// <summary>
+        /// Gets or sets the ID of the principal.
+        /// </summary>
+        [DataMember(Name = "principalId")]
+        public string PrincipalID { get; set; }
+
+        /// <summary>
+        /// Gets or sets the <see cref="APIGatewayCustomAuthorizerPolicy"/> policy document.
+        /// </summary>
+        [DataMember(Name = "policyDocument")]
+        public APIGatewayCustomAuthorizerPolicy PolicyDocument { get; set; } = new APIGatewayCustomAuthorizerPolicy();
+
+        /// <summary>
+        /// Gets or sets the <see cref="APIGatewayCustomAuthorizerContext"/> property.
+        /// </summary>
+        [DataMember(Name = "context")]
+        public APIGatewayCustomAuthorizerContext Context { get; set; }
+    }
+}
