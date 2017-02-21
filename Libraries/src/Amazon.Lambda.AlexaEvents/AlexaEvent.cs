f73a90787f1b16981749144e5984bed3304589ae
Kyle Galbraith <kyle.galbraith459@gmail.com>
2017-02-20 20:19:56 -0800

    add alexa event

--- Libraries/src/Amazon.Lambda.AlexaEvents/AlexaEvent.cs
+++ Libraries/src/Amazon.Lambda.AlexaEvents/AlexaEvent.cs
@@ -5,10 +5,156 @@ using System.Threading.Tasks;
 
 namespace Amazon.Lambda.AlexaEvents
 {
+    /// <summary>
+    /// Alexa Event
+    /// </summary>
     public class AlexaEvent
     {
-        public AlexaEvent()
+        /// <summary>
+        /// The event version.
+        /// </summary>
+        public string EventVersion { get; set; }
+
+        /// <summary>
+        /// The session.
+        /// </summary>
+        public SessionRecord Session { get; set; }
+
+        /// <summary>
+        /// The request.
+        /// </summary>
+        public RequestRecord Request { get; set; }
+
+        /// <summary>
+        /// A session record.
+        /// </summary>
+        public class SessionRecord
         {
+            /// <summary>
+            /// The session id.
+            /// </summary>
+            public string SessionId { get; set; }
+
+            /// <summary>
+            /// The application.
+            /// </summary>
+            public Application Application { get; set; }
+
+            /// <summary>
+            /// The attributes.
+            /// </summary>
+            public Attribute Attributes { get; set; }
+
+            /// <summary>
+            /// The user.
+            /// </summary>
+            public User User { get; set; }
+
+            /// <summary>
+            /// The new flag.
+            /// </summary>
+            public bool New { get; set; }
+        }
+
+        /// <summary>
+        /// A request record.
+        /// </summary>
+        public class RequestRecord
+        {
+            /// <summary>
+            /// The type of request.
+            /// </summary>
+            public string Type { get; set; }
+
+            /// <summary>
+            /// The request id.
+            /// </summary>
+            public string RequestId { get; set; }
+
+            /// <summary>
+            /// The locale.
+            /// </summary>
+            public string Locale { get; set; }
+
+            /// <summary>
+            /// The timestamp.
+            /// </summary>
+            public DateTime Timestamp { get; set; }
+
+            /// <summary>
+            /// The intent.
+            /// </summary>
+            public Intent Intent { get; set; }
+        }
+
+        /// <summary>
+        /// An intent.
+        /// </summary>
+        public class Intent
+        {
+            /// <summary>
+            /// Name of intent.
+            /// </summary>
+            public string Name { get; set; }
+
+            /// <summary>
+            /// Slots for intent.
+            /// </summary>
+            public Slot Slots { get; set; }
+        }
+
+        /// <summary>
+        /// A slot.
+        /// </summary>
+        public class Slot
+        {
+            /// <summary>
+            /// The key.
+            /// </summary>
+            public string Key { get; set; }
+
+            /// <summary>
+            /// The value.
+            /// </summary>
+            public string Value { get; set; }
+        }
+
+        /// <summary>
+        /// An application record.
+        /// </summary>
+        public class Application
+        {
+            /// <summary>
+            /// The application id.
+            /// </summary>
+            public string ApplicationId { get; set; }
+        }
+
+        /// <summary>
+        /// An attribute.
+        /// </summary>
+        public class Attribute
+        {
+            /// <summary>
+            /// The type of attribute.
+            /// </summary>
+            public string Type { get; set; }
+
+            /// <summary>
+            /// The value of attribute
+            /// </summary>
+            public string Value { get; set; }
+        }
+
+        /// <summary>
+        /// A user.
+        /// </summary>
+        public class User
+        {
+            /// <summary>
+            /// The user id.
+            /// </summary>
+            public string UserId { get; set; }
         }
     }
 }
---
+++ Libraries/src/Amazon.Lambda.AlexaEvents/README.md
@@ -0,0 +1,17 @@
+# Amazon.Lambda.AlexaEvents
+
+This package contains classes that can be used as input types for Lambda functions that process Amazon Alexa events. 
+
+# Sample Function
+
+Below is a sample class and Lambda function that illustrates how a AlexaEvents can be used. The function logs an Alexa intent and the slots associated with it (Note that by default anything written to Console will be logged as CloudWatch Logs events.)
+
+```
+public class Function
+{
+    public string Handler(AlexaEvent alexaEvent)
+    {
+		Console.WriteLine($"Intent = {alexaEvent.Intent}; Slots = {alexaEvent.Slots}");
+    }
+}
+```
