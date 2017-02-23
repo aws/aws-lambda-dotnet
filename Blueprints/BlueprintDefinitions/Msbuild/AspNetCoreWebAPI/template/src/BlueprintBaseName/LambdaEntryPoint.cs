ba52327f41b80e3f09233c4bcff5a01c25692e24
Norm Johanson <normj@amazon.com>
2017-02-23 01:02:51 -0800

    Start work on porting AspNetCoreWebAPI

---
+++ Blueprints/BlueprintDefinitions/Msbuild/AspNetCoreWebAPI/template/src/BlueprintBaseName/LambdaEntryPoint.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace BlueprintBaseName
+{
+    /// <summary>
+    /// This class extends from APIGatewayProxyFunction which contains the method FunctionHandlerAsync which is the 
+    /// actual Lambda function entry point. The Lambda handler field should be set to
+    /// 
+    /// BlueprintBaseName::BlueprintBaseName.LambdaEntryPoint::FunctionHandlerAsync
+    /// </summary>
+    public class LambdaEntryPoint : Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction
+    {
+        protected override void Init(IWebHostBuilder builder)
+        {
+            builder
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .UseApiGateway();
+        }
+    }
+}
