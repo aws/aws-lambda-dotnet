eb75b711e7caf2571d547a355d11313eb6be4cc8
Norm Johanson <normj@amazon.com>
2017-02-27 15:00:25 -0800

    Make sure to not include bin and obj folders in blueprints

---
+++ Blueprints/BlueprintPackager/Utilities.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+
+namespace Packager
+{
+    public static class Utilities
+    {
+        public static void ZipCode(string sourceDirectory, string zipArchivePath)
+        {
+            sourceDirectory = sourceDirectory.Replace("\\", "/");
+
+            using (var stream = File.Open(zipArchivePath, FileMode.Create, FileAccess.Write))
+            using (var archive = new ZipArchive(stream, ZipArchiveMode.Create))
+            {
+                foreach (var file in Directory.GetFiles(sourceDirectory, "*", SearchOption.AllDirectories))
+                {
+                    var normalized = file.Replace("\\", "/");
+                    var relativePath = normalized.Substring(sourceDirectory.Length);
+                    if (relativePath.StartsWith("/"))
+                    {
+                        relativePath = relativePath.Substring(1);
+                    }
+
+                    if (relativePath.StartsWith("bin/") ||
+                        relativePath.Contains("/bin/") ||
+                        relativePath.StartsWith("obj/") ||
+                        relativePath.Contains("/obj/"))
+                        continue;
+
+                    var entry = archive.CreateEntry(relativePath);
+                    using (var fileStream = File.OpenRead(file))
+                    using (var entryStream = entry.Open())
+                    {
+                        fileStream.CopyTo(entryStream);
+                    }
+                }
+            }
+        }
+    }
+}
