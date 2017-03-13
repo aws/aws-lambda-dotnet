2ec11b6e25048879a6cbb87adbf0276b5de99187
Norm Johanson <normj@amazon.com>
2017-03-13 00:31:29 -0700

    Work on package and assembly meta data

--- Libraries/src/Amazon.Lambda.S3Events/Properties/AssemblyInfo.cs
+++ Libraries/src/Amazon.Lambda.S3Events/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Lambda event interfaces for S3 event source.")]
 [assembly: AssemblyProduct("Amazon Web Services Lambda Interface for .NET")]
 [assembly: AssemblyCompany("Amazon.com, Inc")]
-[assembly: AssemblyCopyright("Copyright 2009-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.")]
+[assembly: AssemblyCopyright("Copyright 2009-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.")]
 [assembly: ComVisible(false)]
 [assembly: System.CLSCompliant(true)]
 [assembly: AssemblyVersion("1.0")]
