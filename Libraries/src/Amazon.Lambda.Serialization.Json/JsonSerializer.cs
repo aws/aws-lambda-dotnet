bc23d6268719be8f8b6d484ab94ef674ecb1c268
Norm Johanson <normj@amazon.com>
2017-04-22 17:21:58 -0700

    Add LAMBDA_NET_SERIALIZER_DEBUG environment to enable the JSON serializer to log the JSON coming and and being sent back to Lambda.

--- Libraries/src/Amazon.Lambda.Serialization.Json/JsonSerializer.cs
+++ Libraries/src/Amazon.Lambda.Serialization.Json/JsonSerializer.cs
@@ -1,25 +1,39 @@
+using System;
+using System.IO;
 using Amazon.Lambda.Core;
 using Newtonsoft.Json;
-using System.IO;
 
 namespace Amazon.Lambda.Serialization.Json
 {
     /// <summary>
     /// Custom ILambdaSerializer implementation which uses Newtonsoft.Json 9.0.1
     /// for serialization.
+    /// 
+    /// <para>
+    /// If the environment variable LAMBDA_NET_SERIALIZER_DEBUG is set to true the JSON coming
+    /// in from Lambda and being sent back to Lambda will be logged.
+    /// </para>
     /// </summary>
     public class JsonSerializer : ILambdaSerializer
     {
+        private const string DEBUG_ENVIRONMENT_VARIABLE_NAME = "LAMBDA_NET_SERIALIZER_DEBUG";
         private Newtonsoft.Json.JsonSerializer serializer;
+        private bool debug;
 
         /// <summary>
         /// Constructs instance of serializer.
         /// </summary>
         public JsonSerializer()
         {
+
             JsonSerializerSettings settings = new JsonSerializerSettings();
             settings.ContractResolver = new AwsResolver();
             serializer = Newtonsoft.Json.JsonSerializer.Create(settings);
+
+            if (string.Equals(Environment.GetEnvironmentVariable(DEBUG_ENVIRONMENT_VARIABLE_NAME), "true", StringComparison.OrdinalIgnoreCase))
+            {
+                this.debug = true;
+            }
         }
 
         /// <summary>
@@ -30,9 +44,24 @@ namespace Amazon.Lambda.Serialization.Json
         /// <param name="responseStream">Output stream.</param>
         public void Serialize<T>(T response, Stream responseStream)
         {
-            StreamWriter writer = new StreamWriter(responseStream);
-            serializer.Serialize(writer, response);
-            writer.Flush();
+            if (debug)
+            {
+                using (StringWriter debugWriter = new StringWriter())
+                {
+                    serializer.Serialize(debugWriter, response);
+                    Console.WriteLine($"Lambda Serialize {response.GetType().FullName}: {debugWriter.ToString()}");
+
+                    StreamWriter writer = new StreamWriter(responseStream);
+                    writer.Write(debugWriter.ToString());
+                    writer.Flush();
+                }
+            }
+            else
+            {
+                StreamWriter writer = new StreamWriter(responseStream);
+                serializer.Serialize(writer, response);
+                writer.Flush();
+            }
         }
 
         /// <summary>
@@ -43,7 +72,18 @@ namespace Amazon.Lambda.Serialization.Json
         /// <returns>Deserialized object from stream.</returns>
         public T Deserialize<T>(Stream requestStream)
         {
-            StreamReader reader = new StreamReader(requestStream);
+            TextReader reader;
+            if (debug)
+            {
+                var json = new StreamReader(requestStream).ReadToEnd();
+                Console.WriteLine($"Lambda Deserialize {typeof(T).FullName}: {json}");
+                reader = new StringReader(json);
+            }
+            else
+            {
+                reader = new StreamReader(requestStream);
+            }
+
             JsonReader jsonReader = new JsonTextReader(reader);
             return serializer.Deserialize<T>(jsonReader);
         }
