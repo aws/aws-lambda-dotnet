0c7907bd45043c965b2595a525ec40c09c5e3132
Norm Johanson <normj@amazon.com>
2018-01-03 21:40:15 -0800

    Update Amazon.Lambda.Tools version number to 2.0.0 and update AWS SDK dependencies

--- Libraries/src/Amazon.Lambda.Tools/Properties/AssemblyInfo.cs
+++ Libraries/src/Amazon.Lambda.Tools/Properties/AssemblyInfo.cs
@@ -9,5 +9,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright 2009-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.")]
 [assembly: ComVisible(false)]
 [assembly: System.CLSCompliant(true)]
-[assembly: AssemblyVersion("1.0")]
-[assembly: AssemblyFileVersion("1.9.0")]
+[assembly: AssemblyVersion("2.0")]
+[assembly: AssemblyFileVersion("2.0.0")]
