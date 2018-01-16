e7cb54f00221b185bf9ab12844b3a1f276755368
Norm Johanson <normj@amazon.com>
2018-01-15 23:18:22 -0800

    Fixed issue with Amazon.Lambda.Tools .NET Core 2.0 version requiring .NET Core 1.0 runtime being installed

--- Libraries/src/Amazon.Lambda.Tools/Properties/AssemblyInfo.cs
+++ Libraries/src/Amazon.Lambda.Tools/Properties/AssemblyInfo.cs
@@ -10,4 +10,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: System.CLSCompliant(true)]
 [assembly: AssemblyVersion("2.0")]
-[assembly: AssemblyFileVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.1")]
