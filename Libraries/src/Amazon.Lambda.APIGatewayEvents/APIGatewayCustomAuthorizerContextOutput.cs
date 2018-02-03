2a0ba78413fd5bc7bbcea61f9c08e930eb21c4fb
Norm Johanson <normj@amazon.com>
2018-02-02 18:00:38 -0800

    Allow custom output parameter for custom authorizers

--- Libraries/src/Amazon.Lambda.APIGatewayEvents/APIGatewayCustomAuthorizerContextOutput.cs
+++ Libraries/src/Amazon.Lambda.APIGatewayEvents/APIGatewayCustomAuthorizerContextOutput.cs
@@ -1,29 +1,31 @@
 ﻿namespace Amazon.Lambda.APIGatewayEvents
 {
+    using System;
+    using System.Collections.Generic;
     using System.Runtime.Serialization;
 
     /// <summary>
     /// An object representing the expected format of an API Gateway custom authorizer response.
     /// </summary>
     [DataContract]
-    public class APIGatewayCustomAuthorizerContextOutput
+    public class APIGatewayCustomAuthorizerContextOutput : Dictionary<string, object>
     {
         /// <summary>
         /// Gets or sets the 'stringKey' property.
         /// </summary>
-        [DataMember(Name = "stringKey", IsRequired = false)]
+        [Obsolete("This property is obsolete. Code should be updated to use the string index property like authorizer[\"stringKey\"]")]
         public string StringKey { get; set; }
 
         /// <summary>
         /// Gets or sets the 'numKey' property.
         /// </summary>
-        [DataMember(Name = "numKey", IsRequired = false)]
+        [Obsolete("This property is obsolete. Code should be updated to use the string index property like authorizer[\"numKey\"]")]
         public int? NumKey { get; set; }
 
         /// <summary>
         /// Gets or sets the 'boolKey' property.
         /// </summary>
-        [DataMember(Name = "boolKey", IsRequired = false)]
+        [Obsolete("This property is obsolete. Code should be updated to use the string index property like authorizer[\"boolKey\"]")]
         public bool? BoolKey { get; set; }
     }
 }
