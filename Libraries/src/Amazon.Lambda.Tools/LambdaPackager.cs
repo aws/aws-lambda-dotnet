2db8bf3120c3660288f20b1cd558b037847dad68
Stuart Lang <stuart.b.lang@gmail.com>
2018-03-03 20:07:05 +0000

    Fixup xml docs

--- Libraries/src/Amazon.Lambda.Tools/LambdaPackager.cs
+++ Libraries/src/Amazon.Lambda.Tools/LambdaPackager.cs
@@ -37,8 +37,9 @@ namespace Amazon.Lambda.Tools
         /// <param name="logger"></param>
         /// <param name="workingDirectory"></param>
         /// <param name="projectLocation"></param>
-        /// <param name="targetFramework"></param>
         /// <param name="configuration"></param>
+        /// <param name="targetFramework"></param>
+        /// <param name="msbuildParameters"></param>
         /// <param name="disableVersionCheck"></param>
         /// <param name="publishLocation"></param>
         /// <param name="zipArchivePath"></param>
