df9e30507d139d7fee959c966ae3693f67464aeb
Thomas Corbière <thomas.corbiere@protonmail.com>
2018-06-16 14:52:20 +0200

    Improves style in EmptyServerless-FSharp.

--- Blueprints/BlueprintDefinitions/Msbuild-NETCore_2_0/EmptyServerless-FSharp/template/src/BlueprintBaseName.1/Function.fs
+++ Blueprints/BlueprintDefinitions/Msbuild-NETCore_2_0/EmptyServerless-FSharp/template/src/BlueprintBaseName.1/Function.fs
@@ -1,10 +1,11 @@
 namespace BlueprintBaseName._1
 
-open System.Net
-open System.Collections.Generic
 
 open Amazon.Lambda.Core
 open Amazon.Lambda.APIGatewayEvents
+
+open System.Net
+
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [<assembly: LambdaSerializer(typeof<Amazon.Lambda.Serialization.Json.JsonSerializer>)>]
@@ -12,21 +13,18 @@ open Amazon.Lambda.APIGatewayEvents
 
 
 type Functions() =
-
     /// <summary>
     /// A Lambda function to respond to HTTP Get methods from API Gateway
     /// </summary>
     /// <param name="request"></param>
     /// <param name="context"></param>
-    /// <returns>The list of blogs</returns>
-    member _this.Get (request: APIGatewayProxyRequest) (context: ILambdaContext) =
-        context.Logger.LogLine(sprintf "Request: %s" request.Path);
+    /// <returns></returns>
+    member __.Get (request: APIGatewayProxyRequest) (context: ILambdaContext) =
+        sprintf "Request: %s" request.Path
+        |> context.Logger.LogLine
 
-        let response = APIGatewayProxyResponse(
-                        StatusCode = (int)HttpStatusCode.OK,
-                        Body = "Hello AWS Serverless",
-                        Headers = Dictionary<string, string>()
-                        )
-        response.Headers.Add("Content-Type", "text/plain")
-
-        response
+        APIGatewayProxyResponse(
+            StatusCode = int HttpStatusCode.OK,
+            Body = "Hello AWS Serverless",
+            Headers = dict [ ("Content-Type", "text/plain") ]
+        )
