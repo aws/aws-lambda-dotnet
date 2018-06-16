6e25b699fa0cee252ff5efea259941658a58455b
Thomas Corbière <thomas.corbiere@protonmail.com>
2018-06-16 15:44:19 +0200

    Improves style in SimpleDynamoDBFunction-FSharp.

--- Blueprints/BlueprintDefinitions/Msbuild-NETCore_2_0/SimpleDynamoDBFunction-FSharp/template/src/BlueprintBaseName.1/Function.fs
+++ Blueprints/BlueprintDefinitions/Msbuild-NETCore_2_0/SimpleDynamoDBFunction-FSharp/template/src/BlueprintBaseName.1/Function.fs
@@ -1,44 +1,43 @@
-﻿namespace BlueprintBaseName.1
+﻿namespace BlueprintBaseName._1
 
-open Newtonsoft.Json
 
 open Amazon.Lambda.Core
 open Amazon.Lambda.DynamoDBEvents
+
 open System.IO
+open Newtonsoft.Json
+
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [<assembly: LambdaSerializer(typeof<Amazon.Lambda.Serialization.Json.JsonSerializer>)>]
 ()
 
-type Function() =
 
-    let jsonSerializer = new JsonSerializer()
+type Function() =
+    let jsonSerializer = JsonSerializer()
 
     /// <summary>
     /// A simple function to print out the DynamoDB stream event
     /// </summary>
-    /// <param name="input"></param>
+    /// <param name="dynamoEvent"></param>
     /// <param name="context"></param>
     /// <returns></returns>
-    member _this.FunctionHandler (dynamoEvent: DynamoDBEvent) (context: ILambdaContext) =
-
-        context.Logger.LogLine(sprintf "Beginning to process %i records..." dynamoEvent.Records.Count)
+    member __.FunctionHandler (dynamoEvent: DynamoDBEvent) (context: ILambdaContext) =
+        sprintf "Beginning to process %i records..." dynamoEvent.Records.Count
+        |> context.Logger.LogLine
 
         let serializeStreamRecord streamRecord =
             use writer = new StringWriter()
             jsonSerializer.Serialize(writer, streamRecord)
             writer.ToString()
 
-        let printRecord (record : DynamoDBEvent.DynamodbStreamRecord)= 
+        let printRecord (record: DynamoDBEvent.DynamodbStreamRecord) =
             context.Logger.LogLine(sprintf "Event ID: %s" record.EventID)
             context.Logger.LogLine(sprintf "Event Name: %s" record.EventName.Value)
-
             context.Logger.LogLine("DynamoDB Record:")
             context.Logger.LogLine(serializeStreamRecord record.Dynamodb)
 
-
-        dynamoEvent.Records 
-            |> Seq.iter(fun x -> printRecord x)
+        dynamoEvent.Records
+        |> Seq.iter printRecord
 
         context.Logger.LogLine("Stream processing complete.")
-            
