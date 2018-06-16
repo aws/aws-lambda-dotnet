6856ad396ec59e16bb3feff06e2f328ab225aa12
Thomas Corbière <thomas.corbiere@protonmail.com>
2018-06-16 16:15:59 +0200

    Improves style in SimpleS3Function-FSharp.

--- Blueprints/BlueprintDefinitions/Msbuild-NETCore_2_0/SimpleS3Function-FSharp/template/test/BlueprintBaseName.1.Tests/FunctionTest.fs
+++ Blueprints/BlueprintDefinitions/Msbuild-NETCore_2_0/SimpleS3Function-FSharp/template/test/BlueprintBaseName.1.Tests/FunctionTest.fs
@@ -1,13 +1,10 @@
-﻿namespace BlueprintBaseName.1.Tests
+﻿namespace BlueprintBaseName._1.Tests
+
 
 open System
 open System.Collections.Generic
-open System.Linq
-open System.Threading.Tasks
 
 open Xunit
-open Amazon.Lambda
-open Amazon.Lambda.Core
 open Amazon.Lambda.TestUtilities
 open Amazon.Lambda.S3Events
 
@@ -16,52 +13,51 @@ open Amazon.S3
 open Amazon.S3.Model
 open Amazon.S3.Util
 
-open BlueprintBaseName.1
+open BlueprintBaseName._1
 
-module FunctionTest =    
 
+module FunctionTest =
     [<Fact>]
     let ``Test getting content type for an event``() = async {
+        use s3Client = new AmazonS3Client(RegionEndpoint.USWest2)
+        let bucketName = sprintf "lambda-blueprint-basename-%i" DateTime.Now.Ticks
+        let key = "text.txt"
 
-        let s3Client = new AmazonS3Client(RegionEndpoint.USWest2)
-
-        let bucketName = sprintf "lambda-blueprint-basename-%i" DateTime.Now.Ticks;
-        let key = "text.txt";
-
-        let! putBucketResponse = s3Client.PutBucketAsync(bucketName) 
-                                    |> Async.AwaitTask
+        let! putBucketResponse =
+            s3Client.PutBucketAsync(bucketName)
+            |> Async.AwaitTask
 
         try
-            let! putObjectResponse = s3Client.PutObjectAsync(new PutObjectRequest(
-                                                                    BucketName = bucketName, 
-                                                                    Key = key,
-                                                                    ContentBody = "sample data"
-                                                                    )
-                                                                ) 
-                                        |> Async.AwaitTask 
+            let! putObjectResponse =
+                PutObjectRequest(
+                    BucketName = bucketName,
+                    Key = key,
+                    ContentBody = "sample data"
+                )
+                |> s3Client.PutObjectAsync
+                |> Async.AwaitTask
 
-            let s3Event = new S3Event(Records = new List<S3EventNotification.S3EventNotificationRecord>())
-            s3Event.Records.Add(new S3EventNotification.S3EventNotificationRecord(
-                                        S3 = new S3EventNotification.S3Entity(
-                                                    Bucket = new S3EventNotification.S3BucketEntity (Name = bucketName),
-                                                    Object = new S3EventNotification.S3ObjectEntity (Key = key)
-                                                    )
-                                        )
-            )
-                                                    
+            let eventRecords = [
+                S3EventNotification.S3EventNotificationRecord(
+                    S3 = S3EventNotification.S3Entity(
+                        Bucket = S3EventNotification.S3BucketEntity (Name = bucketName),
+                        Object = S3EventNotification.S3ObjectEntity (Key = key)
+                    )
+                )
+            ]
 
-            let lambdaContext = new TestLambdaContext()
-            let lambdaFunction = new Function(s3Client)
+            let s3Event = S3Event(Records = List(eventRecords))
+            let lambdaContext = TestLambdaContext()
+            let lambdaFunction = Function(s3Client)
             let contentType = lambdaFunction.FunctionHandler s3Event lambdaContext
 
-            Assert.Equal("text/plain", contentType);
+            Assert.Equal("text/plain", contentType)
 
         finally
-            AmazonS3Util.DeleteS3BucketWithObjectsAsync(s3Client, bucketName) 
-                |> Async.AwaitTask 
-                |> Async.RunSynchronously
+        AmazonS3Util.DeleteS3BucketWithObjectsAsync(s3Client, bucketName)
+        |> Async.AwaitTask
+        |> Async.RunSynchronously
     }
 
-    
     [<EntryPoint>]
-    let main argv = 0
+    let main _ = 0
