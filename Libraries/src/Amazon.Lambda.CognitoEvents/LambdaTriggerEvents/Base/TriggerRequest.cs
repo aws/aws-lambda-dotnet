15c984537397539c0ecf152e61c3a73261bf8620
GuzzoLM <lucasmguzzo@gmail.com>
2020-09-17 14:47:02 +0100

    Document base parameters

--- Libraries/src/Amazon.Lambda.CognitoEvents/LambdaTriggerEvents/Base/TriggerRequest.cs
+++ Libraries/src/Amazon.Lambda.CognitoEvents/LambdaTriggerEvents/Base/TriggerRequest.cs
@@ -2,8 +2,14 @@
 
 namespace Amazon.Lambda.CognitoEvents.LambdaTriggerEvents.Base
 {
+    /// <summary>
+    /// The request from the Amazon Cognito service.
+    /// </summary>
     public abstract class TriggerRequest
     {
+        /// <summary>
+        /// One or more pairs of user attribute names and values. Each pair is in the form "name": "value".
+        /// </summary>
         public IDictionary<string, string> UserAttributes { get; set; }
     }
 }
