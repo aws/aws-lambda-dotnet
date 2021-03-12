48ca5482779d92be40bdb987c8682067f5ed875f
Ben Cross <b.cross@lancaster.ac.uk>
2021-03-12 13:13:43 +0000

    Add ActiveContexts and AlternativeIntents to LexEvent

--- Libraries/src/Amazon.Lambda.LexEvents/LexEvent.cs
+++ Libraries/src/Amazon.Lambda.LexEvents/LexEvent.cs
@@ -11,7 +11,7 @@ namespace Amazon.Lambda.LexEvents
     public class LexEvent
     {
         /// <summary>
-        /// The version of the message that identifies the format of the event data going into the 
+        /// The version of the message that identifies the format of the event data going into the
         /// Lambda function and the expected format of the response from a Lambda function.
         /// </summary>
         public string MessageVersion { get; set; }
@@ -30,16 +30,16 @@ namespace Amazon.Lambda.LexEvents
         /// The text used to process the request.
         /// </summary>
         public string InputTranscript { get; set; }
-        
+
         /// <summary>
-        /// Application-specific session attributes that the client sent in the request. If you want 
-        /// Amazon Lex to include them in the response to the client, your Lambda function should 
+        /// Application-specific session attributes that the client sent in the request. If you want
+        /// Amazon Lex to include them in the response to the client, your Lambda function should
         /// send these back to Amazon Lex in response.
         /// </summary>
         public IDictionary<string, string> SessionAttributes { get; set; }
 
         /// <summary>
-        /// Request-specific attributes that the client sends in the request. Use request attributes to 
+        /// Request-specific attributes that the client sends in the request. Use request attributes to
         /// pass information that doesn't need to persist for the entire session.
         /// </summary>
         public IDictionary<string, string> RequestAttributes { get; set; }
@@ -50,12 +50,12 @@ namespace Amazon.Lambda.LexEvents
         public LexBot Bot { get; set; }
 
         /// <summary>
-        /// For each user input, the client sends the request to Amazon Lex using one of the runtime API operations, 
-        /// PostContent or PostText. From the API request parameters, Amazon Lex determines whether the response 
+        /// For each user input, the client sends the request to Amazon Lex using one of the runtime API operations,
+        /// PostContent or PostText. From the API request parameters, Amazon Lex determines whether the response
         /// to the client (user) is text or voice, and sets this field accordingly.
         /// <para>
-        /// The Lambda function can use this information to generate an appropriate message. 
-        /// For example, if the client expects a voice response, your Lambda function could return 
+        /// The Lambda function can use this information to generate an appropriate message.
+        /// For example, if the client expects a voice response, your Lambda function could return
         /// Speech Synthesis Markup LanguageSpeech Synthesis Markup Language (SSML) instead of text.
         /// </para>
         /// </summary>
@@ -66,6 +66,67 @@ namespace Amazon.Lambda.LexEvents
         /// </summary>
         public LexCurrentIntent CurrentIntent { get; set; }
 
+        /// <summary>
+        /// A List of alternative intents that are returned when the bot is in advanced mode
+        /// </summary>
+        public IList<LexCurrentIntent> AlternativeIntents { get; set; }
+
+        /// <summary>
+        /// Gets and sets the property ActiveContexts.
+        /// A list of active contexts for the session.A context can be set when an intent is fulfilled or by calling the PostContent, PostText, or PutSession operation.
+        /// You can use a context to control the intents that can follow up an intent, or to modify the operation of your application.
+        /// </summary>
+        public IList<LexActiveContext> ActiveContexts { get; set; }
+
+        /// <summary>
+        /// Gets and sets the property NluIntentConfidence.
+        /// Provides a score that indicates how confident Amazon Lex is that the returned intent is the one that matches the user's intent.
+        /// </summary>
+        public class LexNLUIntentConfidence
+        {
+            /// <summary>
+            /// The score is between 0.0 and 1.0.
+            /// The score is a relative score, not an absolute score.The score may change based on improvements to Amazon Lex.
+            /// </summary>
+            public float Score { get; set; }
+        }
+
+        /// <summary>
+        /// One or more contexts that are active during this turn of a conversation with the user.
+        /// </summary>
+        public class LexActiveContext
+        {
+            /// <summary>
+            /// The length of time or number of turns in the conversation with the user that the context remains active.
+            /// </summary>
+            public TimeToLive TimeToLive { get; set; }
+
+            /// <summary>
+            /// The name of the context.
+            /// </summary>
+            public string Name { get; set; }
+
+            /// <summary>
+            /// A list of key/value pairs the contains the name and value of the slots from the intent that activated the context.
+            /// </summary>
+            public IDictionary<string, string> Parameters { get; set; }
+        }
+
+        /// <summary>
+        /// The length of time or number of turns in the conversation with the user that the context remains active.
+        /// </summary>
+        public class TimeToLive
+        {
+            /// <summary>
+            /// The length of time that the context remains active.
+            /// </summary>
+            public int TimeToLiveInSeconds { get; set; }
+
+            /// <summary>
+            /// The number of turns in the conversation with the user that the context remains active.
+            /// </summary>
+            public int TurnsToLive { get; set; }
+        }
 
         /// <summary>
         /// The class representing the current intent for the Lambda function to process.
@@ -78,10 +139,15 @@ namespace Amazon.Lambda.LexEvents
             public string Name { get; set; }
 
             /// <summary>
-            /// List of slots that are configured for the intent and values that are recognized by 
+            /// List of slots that are configured for the intent and values that are recognized by
             /// Amazon Lex in the user conversation from the beginning. Otherwise, the values are null.
             /// </summary>
             public IDictionary<string, string> Slots { get; set; }
+
+            /// <summary>
+            /// Gets and sets the property NluIntentConfidence.
+            /// </summary>
+            public LexNLUIntentConfidence NLUIntentConfidence { get; set; }
 
             /// <summary>
             /// Provides additional information about a slot value.
@@ -89,7 +155,7 @@ namespace Amazon.Lambda.LexEvents
             public IDictionary<string, SlotDetail> SlotDetails { get; set; }
 
             /// <summary>
-            /// The ConfirmationStatus provides the user response to a confirmation prompt, if there is one. 
+            /// The ConfirmationStatus provides the user response to a confirmation prompt, if there is one.
             /// </summary>
             public string ConfirmationStatus { get; set; }
         }
