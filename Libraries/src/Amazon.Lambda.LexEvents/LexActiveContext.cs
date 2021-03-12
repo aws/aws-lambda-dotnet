05be41d87744665effcef79ff4b5a10661075ad7
Ben Cross <b.cross@lancaster.ac.uk>
2021-03-12 13:38:35 +0000

    Move LexActiveContext to its own file

---
+++ Libraries/src/Amazon.Lambda.LexEvents/LexActiveContext.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Amazon.Lambda.LexEvents
+{
+    /// <summary>
+    /// One or more contexts that are active during this turn of a conversation with the user.
+    /// </summary>
+    [DataContract]
+    public class LexActiveContext
+    {
+        /// <summary>
+        /// The length of time or number of turns in the conversation with the user that the context remains active.
+        /// </summary>
+        [DataMember(Name = "timeToLive", EmitDefaultValue = false)]
+#if NETCOREAPP3_1
+        [System.Text.Json.Serialization.JsonPropertyName("timeToLive")]
+#endif
+        public TimeToLive TimeToLive { get; set; }
+
+        /// <summary>
+        /// The name of the context.
+        /// </summary>
+        [DataMember(Name = "name", EmitDefaultValue = false)]
+#if NETCOREAPP3_1
+        [System.Text.Json.Serialization.JsonPropertyName("name")]
+#endif
+        public string Name { get; set; }
+
+        /// <summary>
+        /// A list of key/value pairs the contains the name and value of the slots from the intent that activated the context.
+        /// </summary>
+        [DataMember(Name = "parameters", EmitDefaultValue = false)]
+#if NETCOREAPP3_1
+        [System.Text.Json.Serialization.JsonPropertyName("parameters")]
+#endif
+        public IDictionary<string, string> Parameters { get; set; }
+    }
+
+    /// <summary>
+    /// The length of time or number of turns in the conversation with the user that the context remains active.
+    /// </summary>
+    [DataContract]
+    public class TimeToLive
+    {
+        /// <summary>
+        /// The length of time that the context remains active.
+        /// </summary>
+        [DataMember(Name = "timeToLiveInSeconds", EmitDefaultValue = false)]
+#if NETCOREAPP3_1
+        [System.Text.Json.Serialization.JsonPropertyName("timeToLiveInSeconds")]
+#endif
+        public int TimeToLiveInSeconds { get; set; }
+
+        /// <summary>
+        /// The number of turns in the conversation with the user that the context remains active.
+        /// </summary>
+        [DataMember(Name = "turnsToLive", EmitDefaultValue = false)]
+#if NETCOREAPP3_1
+        [System.Text.Json.Serialization.JsonPropertyName("turnsToLive")]
+#endif
+        public int TurnsToLive { get; set; }
+    }
+}
--- Libraries/src/Amazon.Lambda.LexEvents/LexEvent.cs
+++ Libraries/src/Amazon.Lambda.LexEvents/LexEvent.cs
@@ -92,43 +92,6 @@ namespace Amazon.Lambda.LexEvents
         }
 
         /// <summary>
-        /// One or more contexts that are active during this turn of a conversation with the user.
-        /// </summary>
-        public class LexActiveContext
-        {
-            /// <summary>
-            /// The length of time or number of turns in the conversation with the user that the context remains active.
-            /// </summary>
-            public TimeToLive TimeToLive { get; set; }
-
-            /// <summary>
-            /// The name of the context.
-            /// </summary>
-            public string Name { get; set; }
-
-            /// <summary>
-            /// A list of key/value pairs the contains the name and value of the slots from the intent that activated the context.
-            /// </summary>
-            public IDictionary<string, string> Parameters { get; set; }
-        }
-
-        /// <summary>
-        /// The length of time or number of turns in the conversation with the user that the context remains active.
-        /// </summary>
-        public class TimeToLive
-        {
-            /// <summary>
-            /// The length of time that the context remains active.
-            /// </summary>
-            public int TimeToLiveInSeconds { get; set; }
-
-            /// <summary>
-            /// The number of turns in the conversation with the user that the context remains active.
-            /// </summary>
-            public int TurnsToLive { get; set; }
-        }
-
-        /// <summary>
         /// The class representing the current intent for the Lambda function to process.
         /// </summary>
         public class LexCurrentIntent
