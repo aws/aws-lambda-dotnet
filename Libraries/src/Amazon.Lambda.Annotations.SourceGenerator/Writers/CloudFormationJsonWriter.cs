092e3883900ebdcdb5aa68c32ff52b66098703ab
Malhar Khimsaria <kmalhar@amazon.com>
2021-11-05 16:52:51 -0400

    fix: Delete redundant event attribute entries from serverless.template.

--- Libraries/src/Amazon.Lambda.Annotations.SourceGenerator/Writers/CloudFormationJsonWriter.cs
+++ Libraries/src/Amazon.Lambda.Annotations.SourceGenerator/Writers/CloudFormationJsonWriter.cs
@@ -83,27 +83,48 @@ namespace Amazon.Lambda.Annotations.SourceGenerator.Writers
                 _jsonWriter.RemoveToken($"{propertiesPath}.Role");
             }
             
-            if (lambdaFunction.Attributes != null && lambdaFunction.Attributes.Any())
-                ProcessLambdaFunctionAttributes(lambdaFunction);
+            ProcessLambdaFunctionEvents(lambdaFunction);
         }
 
-        private void ProcessLambdaFunctionAttributes(ILambdaFunctionSerializable lambdaFunction)
+        private void ProcessLambdaFunctionEvents(ILambdaFunctionSerializable lambdaFunction)
         {
+            var currentSyncedEvents = new List<string>();
+            string eventName;
+            
             foreach (var attributeModel in lambdaFunction.Attributes)
             {
                 switch (attributeModel)
                 {
                     case AttributeModel<HttpApiAttribute> httpApiAttributeModel:
-                        ProcessHttpApiAttribute(lambdaFunction, httpApiAttributeModel.Data);
+                        eventName = ProcessHttpApiAttribute(lambdaFunction, httpApiAttributeModel.Data);
+                        currentSyncedEvents.Add(eventName);
                         break;
                     case AttributeModel<RestApiAttribute> restApiAttributeModel:
-                        ProcessRestApiAttribute(lambdaFunction, restApiAttributeModel.Data);
+                        eventName = ProcessRestApiAttribute(lambdaFunction, restApiAttributeModel.Data);
+                        currentSyncedEvents.Add(eventName);
                         break;
                 }
             }
+
+            var eventsPath = $"Resources.{lambdaFunction.Name}.Properties.Events";
+            var syncedEventsMetadataPath = $"Resources.{lambdaFunction.Name}.Metadata.SyncedEvents";
+            
+            if (_jsonWriter.GetToken(syncedEventsMetadataPath, new JArray()) is JArray previousSyncedEvents)
+            {
+                foreach (var previousEventName in previousSyncedEvents.Select(x => x.ToObject<string>()))
+                {
+                    if (!currentSyncedEvents.Contains(previousEventName))
+                        _jsonWriter.RemoveToken($"{eventsPath}.{previousEventName}");
+                }
+            }
+
+            if (currentSyncedEvents.Any())
+                _jsonWriter.SetToken(syncedEventsMetadataPath, new JArray(currentSyncedEvents));
+            else
+                _jsonWriter.RemoveToken(syncedEventsMetadataPath);
         }
 
-        private void ProcessRestApiAttribute(ILambdaFunctionSerializable lambdaFunction, RestApiAttribute restApiAttribute)
+        private string ProcessRestApiAttribute(ILambdaFunctionSerializable lambdaFunction, RestApiAttribute restApiAttribute)
         {
             var eventPath = $"Resources.{lambdaFunction.Name}.Properties.Events";
             var methodName = restApiAttribute.Method.ToString();
@@ -112,9 +133,11 @@ namespace Amazon.Lambda.Annotations.SourceGenerator.Writers
             _jsonWriter.SetToken($"{methodPath}.Type", "Api");
             _jsonWriter.SetToken($"{methodPath}.Properties.Path", restApiAttribute.Template);
             _jsonWriter.SetToken($"{methodPath}.Properties.Method", methodName.ToUpper());
+
+            return $"Root{methodName}";
         }
 
-        private void ProcessHttpApiAttribute(ILambdaFunctionSerializable lambdaFunction, HttpApiAttribute httpApiAttribute)
+        private string ProcessHttpApiAttribute(ILambdaFunctionSerializable lambdaFunction, HttpApiAttribute httpApiAttribute)
         {
             var eventPath = $"Resources.{lambdaFunction.Name}.Properties.Events";
             var methodName = httpApiAttribute.Method.ToString();
@@ -125,6 +148,8 @@ namespace Amazon.Lambda.Annotations.SourceGenerator.Writers
             _jsonWriter.SetToken($"{methodPath}.Properties.Path", httpApiAttribute.Template);
             _jsonWriter.SetToken($"{methodPath}.Properties.Method", methodName.ToUpper());
             _jsonWriter.SetToken($"{methodPath}.Properties.PayloadFormatVersion", version);
+            
+            return $"Root{methodName}";
         }
         
         private void ApplyLambdaFunctionDefaults(string lambdaFunctionPath, string propertiesPath)
