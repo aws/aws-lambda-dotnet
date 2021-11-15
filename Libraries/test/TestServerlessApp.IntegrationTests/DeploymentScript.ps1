f60689325fe66dc3f246fc6f921b755a218cd751
Malhar Khimsaria <kmalhar@amazon.com>
2021-11-15 16:08:15 -0500

    fix: Fix DeploymentScript path in csproj file.

--- Libraries/test/TestServerlessApp.IntegrationTests/DeploymentScript.ps1
+++ Libraries/test/TestServerlessApp.IntegrationTests/DeploymentScript.ps1
@@ -4,7 +4,7 @@ $stackName = "test-serverless-app-" + $suffix
 $bucketName = "devex-test-serverless-app-" + $suffix
 & cd ..\TestServerlessApp
 & dotnet tool install -g Amazon.Lambda.Tools
-& aws s3api create-bucket --bucket $bucketName --create-bucket-configuration LocationConstraint=us-west-2
+& aws s3 mb s3://$bucketName
 & dotnet restore
 & dotnet lambda deploy-serverless $stackName --s3-bucket $bucketName
 & cd ..\TestServerlessApp.IntegrationTests
