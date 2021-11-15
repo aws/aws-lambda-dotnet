5476561035b3137d6a5f282e7b374e5da93b3612
Malhar Khimsaria <kmalhar@amazon.com>
2021-11-15 17:05:32 -0500

    fix: edit DeploymentScript

--- Libraries/test/TestServerlessApp.IntegrationTests/DeploymentScript.ps1
+++ Libraries/test/TestServerlessApp.IntegrationTests/DeploymentScript.ps1
@@ -1,11 +1,37 @@
-$guid = New-Guid
-$suffix = $guid.ToString().Split('-') | Select-Object -First 1
-$stackName = "test-serverless-app-" + $suffix
-$bucketName = "devex-test-serverless-app-" + $suffix
-& cd ..\TestServerlessApp
-& dotnet tool install -g Amazon.Lambda.Tools
-& aws s3 mb s3://$bucketName
-& dotnet restore
-& dotnet lambda deploy-serverless $stackName --s3-bucket $bucketName
-& cd ..\TestServerlessApp.IntegrationTests
-New-Item -Path . -Name "parameters.txt" -ItemType "file" -Value "stackName=$stackName`nbucketName=$bucketName" -Force
+$ErrorActionPreference = 'Stop'
+try
+{
+    Push-Location $PSScriptRoot
+    $guid = New-Guid
+    $suffix = $guid.ToString().Split('-') | Select-Object -First 1
+    $identifier = "test-serverless-app-" + $suffix
+    & cd ..\TestServerlessApp
+    if (!$?)
+    {
+        throw "Failed to tag. SourceTag: ${SourceAmd64Uri}, TargetTag: ${Amd64Uri}"
+    }
+    dotnet tool install -g Amazon.Lambda.Tools
+    Write-Host "Creating Bucket $identifier..."
+    aws s3 mb s3://$identifier
+    if (!$?)
+    {
+        throw "Failed to tag. SourceTag: ${SourceAmd64Uri}, TargetTag: ${Amd64Uri}"
+    }
+    dotnet restore
+    Write-Host "Creating CloudFormation Stack $identifier..."
+    dotnet lambda deploy-serverless $identifier --s3-bucket $identifier
+    if (!$?)
+    {
+        throw "Failed to tag. SourceTag: ${SourceAmd64Uri}, TargetTag: ${Amd64Uri}"
+    }
+    cd ..\TestServerlessApp.IntegrationTests
+    if (!$?)
+    {
+        throw "Failed to tag. SourceTag: ${SourceAmd64Uri}, TargetTag: ${Amd64Uri}"
+    }
+    New-Item -Path . -Name "parameters.txt" -ItemType "file" -Value "stackName=$identifier`nbucketName=$identifier" -Force
+}
+finally
+{
+    Pop-Location
+}
