6eb7735e865d9f9dc11307cd7cae7ccd68fb482a
edwok <fisheroliver9@gmail.com>
2021-11-18 16:36:14 +0100

    feature: Add a Kafka event type for the Lambda
    # Conflicts:
    #	Libraries/Libraries.sln

---
+++ Libraries/src/Amazon.Lambda.KafkaEvents/KafkaEvent.cs
@@ -0,0 +1,73 @@
+﻿namespace Amazon.Lambda.KafkaEvents
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Apache Kafka event
+    /// https://docs.aws.amazon.com/lambda/latest/dg/with-msk.html
+    /// https://docs.aws.amazon.com/lambda/latest/dg/with-kafka.html
+    /// </summary>
+    public class KafkaEvent
+    {
+        /// <summary>
+        /// The source of the event.
+        /// </summary>
+        public string EventSource { get; set; }
+
+        /// <summary>
+        /// The ARN of the event source.
+        /// </summary>
+        public string EventSourceARN { get; set; }
+
+        /// <summary>
+        /// List of Kafka event records.
+        /// </summary>
+        public IDictionary<string, IList<KafkaEventRecord>> Records { get; set; }
+
+        /// <summary>
+        /// Kafka event record.
+        /// </summary>
+        public class KafkaEventRecord
+        {
+            /// <summary>
+            /// The topic associated with the event record.
+            /// </summary>
+            public string Topic { get; set; }
+
+            /// <summary>
+            /// The partition associated with the event record.
+            /// </summary>
+            public int Partition { get; set; }
+
+            /// <summary>
+            /// The partition offset associated with the event record.
+            /// </summary>
+            public long Offset { get; set; }
+
+            /// <summary>
+            /// The Kafka event record timestamp.
+            /// </summary>
+            public long Timestamp { get; set; }
+
+            /// <summary>
+            /// The Kafka event record timestamp type.
+            /// </summary>
+            public string TimestampType { get; set; }
+
+            /// <summary>
+            /// The Kafka event record Key.
+            /// </summary>
+            public string Key { get; set; }
+
+            /// <summary>
+            /// The Kafka event record Value.
+            /// </summary>
+            public string Value { get; set; }
+
+            /// <summary>
+            /// The Kafka event record headers.
+            /// </summary>
+            public List<Dictionary<string, byte[]>> Headers { get; set; }
+        }
+    }
+}
