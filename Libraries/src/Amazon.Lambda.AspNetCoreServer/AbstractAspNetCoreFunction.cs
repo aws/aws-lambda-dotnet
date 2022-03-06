572ceb4c6925184542f921da26b4e6805c28a720
martincostello <martin@martincostello.com>
2022-03-06 11:08:54 +0000

    Support image/x-icon as base64
    Support "image/x-icon" for favicons as base64 binary content out-of-the-box.

--- Libraries/src/Amazon.Lambda.AspNetCoreServer/AbstractAspNetCoreFunction.cs
+++ Libraries/src/Amazon.Lambda.AspNetCoreServer/AbstractAspNetCoreFunction.cs
@@ -72,6 +72,7 @@ namespace Amazon.Lambda.AspNetCoreServer
             ["image/gif"] = ResponseContentEncoding.Base64,
             ["image/jpeg"] = ResponseContentEncoding.Base64,
             ["image/jpg"] = ResponseContentEncoding.Base64,
+            ["image/x-icon"] = ResponseContentEncoding.Base64,
             ["application/zip"] = ResponseContentEncoding.Base64,
             ["application/pdf"] = ResponseContentEncoding.Base64,
         };
