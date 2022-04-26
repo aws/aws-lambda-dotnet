bfaa39944c029336ef0cf138c83bee1b17be1268
Norm Johanson <normj@amazon.com>
2022-04-26 00:27:55 -0700

    Fixed bug with Amazon.Lambda.Annotations not correctly using assembly name for computing function handler string

---
+++ Libraries/test/Amazon.Lambda.Annotations.SourceGenerators.Tests/Snapshots/Functions_ToUpper_Generated.g.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using Amazon.Lambda.Core;
+
+namespace TestServerlessApp.Sub1
+{
+    public class Functions_ToUpper_Generated
+    {
+        private readonly Functions functions;
+
+        public Functions_ToUpper_Generated()
+        {
+            SetExecutionEnvironment();
+            functions = new Functions();
+        }
+
+        public string ToUpper(string text)
+        {
+            return functions.ToUpper(text);
+        }
+
+        private static void SetExecutionEnvironment()
+        {
+            const string envName = "AWS_EXECUTION_ENV";
+
+            var envValue = new StringBuilder();
+
+            // If there is an existing execution environment variable add the annotations package as a suffix.
+            if(!string.IsNullOrEmpty(Environment.GetEnvironmentVariable(envName)))
+            {
+                envValue.Append($"{Environment.GetEnvironmentVariable(envName)}_");
+            }
+
+            envValue.Append("amazon-lambda-annotations_0.5.1.0");
+
+            Environment.SetEnvironmentVariable(envName, envValue.ToString());
+        }
+    }
+}
