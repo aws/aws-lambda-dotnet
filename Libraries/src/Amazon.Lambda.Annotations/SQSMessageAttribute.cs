79e39c072804b25749c6447d895d3530093dd3da
Tim <hounddog@gmail.com>
2022-06-13 03:08:00 -0400

    remove unnecessary comments from concrete passes thru from interface

--- Libraries/src/Amazon.Lambda.Annotations/SQSMessageAttribute.cs
+++ Libraries/src/Amazon.Lambda.Annotations/SQSMessageAttribute.cs
@@ -209,13 +209,6 @@ namespace Amazon.Lambda.Annotations
         public string[] Tags { get; set; } = new string[] {};
         public int VisibilityTimeout { get; set; } = VisibilityTimeoutDefault;
         public int ReceiveMessageWaitTimeSeconds { get; set; } = ReceiveMessageWaitTimeSecondsDefault;
-
-        /// <summary>
-        /// For first-in-first-out (FIFO) queues, specifies whether to enable content-based deduplication. During the deduplication interval, Amazon SQS treats messages that are sent with identical content as duplicates and delivers only one copy of the message. For more information, see the ContentBasedDeduplication attribute for the CreateQueue action in the Amazon SQS API Reference.
-        /// Required: No
-        /// Type: Boolean
-        /// Update requires: No interruption
-        /// </summary>
         public bool ContentBasedDeduplication { get; set; } = ContentBasedDeduplicationDefault;
         public string DeduplicationScope { get; set; }
         public int DelaySeconds { get; set; } = DelaySecondsDefault;
