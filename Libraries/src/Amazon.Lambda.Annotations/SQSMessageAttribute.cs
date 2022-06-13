5fb93a15ad2c11ea7cb881984d2ed51089e72eb7
Tim <hounddog@gmail.com>
2022-06-13 02:44:35 -0400

    move comment to concrete for usability

--- Libraries/src/Amazon.Lambda.Annotations/SQSMessageAttribute.cs
+++ Libraries/src/Amazon.Lambda.Annotations/SQSMessageAttribute.cs
@@ -11,13 +11,6 @@ namespace Amazon.Lambda.Annotations
         string QueueLogicalId { get; set; }
         string Queue { get; set; }
 
-
-        /// <summary>
-        /// For first-in-first-out (FIFO) queues, specifies whether to enable content-based deduplication. During the deduplication interval, Amazon SQS treats messages that are sent with identical content as duplicates and delivers only one copy of the message. For more information, see the ContentBasedDeduplication attribute for the CreateQueue action in the Amazon SQS API Reference.
-        /// Required: No
-        /// Type: Boolean
-        /// Update requires: No interruption
-        /// </summary>
         bool ContentBasedDeduplication { get; set; }
 
         /// <summary>
@@ -188,6 +181,13 @@ namespace Amazon.Lambda.Annotations
         public string[] Tags { get; set; } = new string[] {};
         public int VisibilityTimeout { get; set; } = VisibilityTimeoutDefault;
         public int ReceiveMessageWaitTimeSeconds { get; set; } = ReceiveMessageWaitTimeSecondsDefault;
+
+        /// <summary>
+        /// For first-in-first-out (FIFO) queues, specifies whether to enable content-based deduplication. During the deduplication interval, Amazon SQS treats messages that are sent with identical content as duplicates and delivers only one copy of the message. For more information, see the ContentBasedDeduplication attribute for the CreateQueue action in the Amazon SQS API Reference.
+        /// Required: No
+        /// Type: Boolean
+        /// Update requires: No interruption
+        /// </summary>
         public bool ContentBasedDeduplication { get; set; } = ContentBasedDeduplicationDefault;
         public string DeduplicationScope { get; set; }
         public int DelaySeconds { get; set; } = DelaySecondsDefault;
