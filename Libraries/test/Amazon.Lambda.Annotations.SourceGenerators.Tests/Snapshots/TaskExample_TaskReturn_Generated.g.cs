d23c760c7edb7a83070f7978dfe0867c31f8a79d
Norm Johanson <normj@amazon.com>
2022-08-25 14:44:22 -0700

    Fix code generation in Lambda Annotations when return a non generic Task

---
+++ Libraries/test/Amazon.Lambda.Annotations.SourceGenerators.Tests/Snapshots/TaskExample_TaskReturn_Generated.g.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using Amazon.Lambda.Core;
+
+namespace TestServerlessApp
+{
+    public class TaskExample_TaskReturn_Generated
+    {
+        private readonly TaskExample taskExample;
+
+        public TaskExample_TaskReturn_Generated()
+        {
+            SetExecutionEnvironment();
+            taskExample = new TaskExample();
+        }
+
+        public async System.Threading.Tasks.Task TaskReturn(string text, Amazon.Lambda.Core.ILambdaContext __context__)
+        {
+            await taskExample.TaskReturn(text, __context__);
+        }
+
+        private static void SetExecutionEnvironment()
+        {
+            const string envName = "AWS_EXECUTION_ENV";
+
+            var envValue = new StringBuilder();
+
+            // If there is an existing execution environment variable add the annotations package as a suffix.
+            if(!string.IsNullOrEmpty(Environment.GetEnvironmentVariable(envName)))
+            {
+                envValue.Append($"{Environment.GetEnvironmentVariable(envName)}_");
+            }
+
+            envValue.Append("amazon-lambda-annotations_0.6.0.0");
+
+            Environment.SetEnvironmentVariable(envName, envValue.ToString());
+        }
+    }
+}
