ea15f4b19353c1918e319769bbcb4449679320cf
Philippe El Asmar <phil.asmar@gmail.com>
2022-09-09 09:38:11 -0400

    Version bump Amazon.Lambda.Annotations to 1.0.0

--- Libraries/test/Amazon.Lambda.Annotations.SourceGenerators.Tests/Snapshots/SimpleCalculator_Pi_Generated.g.cs
+++ Libraries/test/Amazon.Lambda.Annotations.SourceGenerators.Tests/Snapshots/SimpleCalculator_Pi_Generated.g.cs
@@ -48,7 +48,7 @@ namespace TestServerlessApp
                 envValue.Append($"{Environment.GetEnvironmentVariable(envName)}_");
             }
 
-            envValue.Append("amazon-lambda-annotations_0.7.0.0");
+            envValue.Append("amazon-lambda-annotations_1.0.0.0");
 
             Environment.SetEnvironmentVariable(envName, envValue.ToString());
         }
