db9adc4043c0cf7be94db3331f123c39983fe1ce
Philippe El Asmar <53088140+philasmar@users.noreply.github.com>
2022-11-23 12:45:24 -0500

    fix: added support for dynamic type in lambda annotations

--- Libraries/src/Amazon.Lambda.Annotations.SourceGenerator/Models/TypeModelBuilder.cs
+++ Libraries/src/Amazon.Lambda.Annotations.SourceGenerator/Models/TypeModelBuilder.cs
@@ -11,14 +11,34 @@ namespace Amazon.Lambda.Annotations.SourceGenerator.Models
     {
         public static TypeModel Build(ITypeSymbol symbol, GeneratorExecutionContext context)
         {
+            bool isGenericType;
+            if (symbol is INamedTypeSymbol)
+                isGenericType = ((INamedTypeSymbol)symbol)?.IsGenericType ?? false;
+            else
+                isGenericType = false;
+
+            IList<TypeModel> typeArguments;
+            if (symbol is INamedTypeSymbol)
+                typeArguments = ((INamedTypeSymbol)symbol)?.TypeArguments.Select(arg => Build(arg, context)).ToList();
+            else
+                typeArguments = new List<TypeModel>();
+
+            bool isEnumerable;
+            if (symbol is INamedTypeSymbol)
+                isEnumerable = ((INamedTypeSymbol)symbol)?.Interfaces.Any(it => it.Equals(context.Compilation.GetTypeByMetadataName(TypeFullNames.IEnumerable), SymbolEqualityComparer.Default)) ?? false;
+            else if (symbol is IDynamicTypeSymbol)
+                isEnumerable = ((IDynamicTypeSymbol)symbol)?.Interfaces.Any(it => it.Equals(context.Compilation.GetTypeByMetadataName(TypeFullNames.IEnumerable), SymbolEqualityComparer.Default)) ?? false;
+            else
+                isEnumerable = false;
+
             var model = new TypeModel
             {
                 Name = symbol.Name,
                 FullName = symbol.ToDisplayString(),
                 IsValueType = symbol.IsValueType,
-                IsGenericType = ((INamedTypeSymbol)symbol)?.IsGenericType ?? false,
-                TypeArguments = ((INamedTypeSymbol)symbol)?.TypeArguments.Select(arg => Build(arg, context)).ToList(),
-                IsEnumerable = ((INamedTypeSymbol)symbol)?.Interfaces.Any(it => it.Equals(context.Compilation.GetTypeByMetadataName(TypeFullNames.IEnumerable), SymbolEqualityComparer.Default)) ?? false
+                IsGenericType = isGenericType,
+                TypeArguments = typeArguments,
+                IsEnumerable = isEnumerable
             };
 
             return model;
