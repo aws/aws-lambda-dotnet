b5af0b91bac58ab0f19d6f0a7aa61230fb07ffea
Pieter <pieter@pieterg.com>
2022-12-01 20:59:33 +0100

    Change spelling of serialziation to serialization

--- Blueprints/BlueprintDefinitions/vs2022/NativeAOTFunction/template/src/BlueprintBaseName.1/Function.cs
+++ Blueprints/BlueprintDefinitions/vs2022/NativeAOTFunction/template/src/BlueprintBaseName.1/Function.cs
@@ -48,7 +48,7 @@ public class Function
 /// <summary>
 /// This class is used to register the input event and return type for the FunctionHandler method with the System.Text.Json source generator.
 /// There must be a JsonSerializable attribute for each type used as the input and return type or a runtime error will occur 
-/// from the JSON serializer unable to find the serialziation information for unknown types.
+/// from the JSON serializer unable to find the serialization information for unknown types.
 /// </summary>
 [JsonSerializable(typeof(string))]
 public partial class LambdaFunctionJsonSerializerContext : JsonSerializerContext
