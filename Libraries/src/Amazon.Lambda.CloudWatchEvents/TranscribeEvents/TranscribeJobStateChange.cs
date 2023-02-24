ec2bbd012813f64a5d6277a85896d72fc9c1711d
Bryan Hogan <bjhogan@amazon.com>
2023-02-24 11:28:48 -0500

    Add Transcribe Job Stage Change Event

---
+++ Libraries/src/Amazon.Lambda.CloudWatchEvents/TranscribeEvents/TranscribeJobStateChange.cs
@@ -0,0 +1,23 @@
+namespace Amazon.Lambda.CloudWatchEvents.TranscribeEvents
+{
+    /// <summary>
+    /// This class represents the details of a Transcribe Job State Change sent via EventBridge.
+    /// </summary>
+    public class TranscribeJobStateChange
+    {
+        /// <summary>
+        /// The transcription job name.
+        /// </summary>
+        public string TranscriptionJobName { get; set; }
+
+        /// <summary>
+        /// The transcription job status.
+        /// </summary>
+        public string TranscriptionJobStatus { get; set; }
+
+        /// <summary>
+        /// If the TranscriptionJobStatus is FAILED, this field contains information about the failure.
+        /// </summary>
+        public string FailureReason { get; set; }
+    }
+}
---
+++ Libraries/src/Amazon.Lambda.CloudWatchEvents/TranscribeEvents/TranscribeJobStateChangeEvent.cs
@@ -0,0 +1,7 @@
+namespace Amazon.Lambda.CloudWatchEvents.TranscribeEvents
+{
+    /// <summary>
+    /// This class represents a Transcribe Job State Change sent via EventBridge.
+    /// </summary>
+    public class TranscribeJobStateChangeEvent : CloudWatchEvent<TranscribeJobStateChange>{ }
+}
