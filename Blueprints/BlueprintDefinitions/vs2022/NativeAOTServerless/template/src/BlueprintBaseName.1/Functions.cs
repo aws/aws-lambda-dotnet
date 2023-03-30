2973d407d2d3181403a05ae9822fd21491c811f2
Beau Gosse <bgosse@amazon.com>
2023-03-30 17:40:43 -0500

    Remove rd.xml from aot blueprints (#1465)

--- Blueprints/BlueprintDefinitions/vs2022/NativeAOTServerless/template/src/BlueprintBaseName.1/Functions.cs
+++ Blueprints/BlueprintDefinitions/vs2022/NativeAOTServerless/template/src/BlueprintBaseName.1/Functions.cs
@@ -49,11 +49,12 @@ public class Functions
     /// defined below will need the JsonSerializable updated. If the return type and event type are different then the 
     /// LambdaFunctionJsonSerializerContext must have two JsonSerializable attributes, one for each type.
     ///
-    /// When using Native AOT, libraries used with your Lambda function might not be compatible with trimming that
-    /// happens as part of the Native AOT compilation. If you find when testing your Native AOT Lambda function that 
-    /// you get runtime errors about missing types, methods or constructors then add the assembly that contains the
-    /// types into the rd.xml file. This will tell the Native AOT compiler to not trim those assemblies. Currently the 
-    /// AWS SDK for .NET does not support trimming and when used should be added to the rd.xml file.    
+    // When using Native AOT extra testing with the deployed Lambda functions is required to ensure
+    // the libraries used in the Lambda function work correctly with Native AOT. If a runtime 
+    // error occurs about missing types or methods the most likely solution will be to remove references to trim-unsafe 
+    // code or configure trimming options. This sample defaults to partial TrimMode because currently the AWS 
+    // SDK for .NET does not support trimming. This will result in a larger executable size, and still does not 
+    // guarantee runtime trimming errors won't be hit. 
     /// </summary>
     /// <param name="request"></param>
     /// <param name="context"></param>
@@ -82,11 +83,12 @@ public class Functions
     /// defined below will need the JsonSerializable updated. If the return type and event type are different then the 
     /// LambdaFunctionJsonSerializerContext must have two JsonSerializable attributes, one for each type.
     ///
-    /// When using Native AOT, libraries used with your Lambda function might not be compatible with trimming that
-    /// happens as part of the Native AOT compilation. If you find when testing your Native AOT Lambda function that 
-    /// you get runtime errors about missing types, methods or constructors then add the assembly that contains the
-    /// types into the rd.xml file. This will tell the Native AOT compiler to not trim those assemblies. Currently the 
-    /// AWS SDK for .NET does not support trimming and when used should be added to the rd.xml file.    
+    // When using Native AOT extra testing with the deployed Lambda functions is required to ensure
+    // the libraries used in the Lambda function work correctly with Native AOT. If a runtime 
+    // error occurs about missing types or methods the most likely solution will be to remove references to trim-unsafe 
+    // code or configure trimming options. This sample defaults to partial TrimMode because currently the AWS 
+    // SDK for .NET does not support trimming. This will result in a larger executable size, and still does not 
+    // guarantee runtime trimming errors won't be hit. 
     /// </summary>
     /// <param name="request"></param>
     /// <param name="context"></param>
