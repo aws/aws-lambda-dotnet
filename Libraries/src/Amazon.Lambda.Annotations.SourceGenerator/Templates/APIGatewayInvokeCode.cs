0499a3c55057add33cae782f750416a240bc49e0
Norm Johanson <normj@amazon.com>
2023-05-01 16:47:48 -0700

    Refactor the monolithic LambdaFunctionTemplate.tt into separate tt files per section being generated.

---
+++ Libraries/src/Amazon.Lambda.Annotations.SourceGenerator/Templates/APIGatewayInvokeCode.cs
@@ -0,0 +1,17 @@
+﻿using Amazon.Lambda.Annotations.SourceGenerator.Models;
+
+namespace Amazon.Lambda.Annotations.SourceGenerator.Templates
+{
+    public partial class APIGatewayInvoke
+    {
+        private readonly LambdaFunctionModel _model;
+
+        public readonly string _parameterSignature;
+
+        public APIGatewayInvoke(LambdaFunctionModel model, string parameterSignature)
+        {
+            _model = model;
+            _parameterSignature = parameterSignature;
+        }
+    }
+}
---
+++ Libraries/src/Amazon.Lambda.Annotations.SourceGenerator/Templates/APIGatewaySetupParametersCode.cs
@@ -0,0 +1,16 @@
+﻿using Amazon.Lambda.Annotations.SourceGenerator.Models;
+
+namespace Amazon.Lambda.Annotations.SourceGenerator.Templates
+{
+    public partial class APIGatewaySetupParameters
+    {
+        private readonly LambdaFunctionModel _model;
+
+        public string ParameterSignature { get; set; }
+
+        public APIGatewaySetupParameters(LambdaFunctionModel model)
+        {
+            _model = model;
+        }
+    }
+}
