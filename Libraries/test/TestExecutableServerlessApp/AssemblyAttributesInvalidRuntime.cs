0207130270838bf42a6ccdf014c4e3de8607be5f
jamesuk <jamesuk@amazon.com>
2023-09-11 13:54:19 +0100

    Update unit tests and documentation

---
+++ Libraries/test/TestExecutableServerlessApp/AssemblyAttributesInvalidRuntime.cs
@@ -0,0 +1,5 @@
+﻿using Amazon.Lambda.Annotations;
+using Amazon.Lambda.Core;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+[assembly: LambdaGlobalProperties(GenerateMain = true, Runtime = "notavalidruntime")]
