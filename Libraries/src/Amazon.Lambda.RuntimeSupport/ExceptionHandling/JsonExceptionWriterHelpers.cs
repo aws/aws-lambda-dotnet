616f384485d04d2b658141b83ad726a0889ea321
Anirudh Singh Rawat <anirudhrawat1@gmail.com>
2023-09-13 17:44:04 -0700

    feat: added json library in `JSONExceptionWriter`
    rewrite the *JsonExceptionWriter classes to use a JSON library instead of building strings directly.

--- Libraries/src/Amazon.Lambda.RuntimeSupport/ExceptionHandling/JsonExceptionWriterHelpers.cs
+++ Libraries/src/Amazon.Lambda.RuntimeSupport/ExceptionHandling/JsonExceptionWriterHelpers.cs
@@ -13,7 +13,7 @@
  * permissions and limitations under the License.
  */
 
-using System.Text;
+using System.Text.Json;
 
 namespace Amazon.Lambda.RuntimeSupport
 {
@@ -22,8 +22,6 @@ namespace Amazon.Lambda.RuntimeSupport
         /// <summary>
         /// This method escapes a string for use as a JSON string value.
         /// It was adapted from the PutString method in the LitJson.JsonWriter class.
-        ///
-        /// TODO: rewrite the *JsonExceptionWriter classes to use a JSON library instead of building strings directly.
         /// </summary>
         /// <param name="str"></param>
         public static string EscapeStringForJson(string str)
@@ -31,87 +29,9 @@ namespace Amazon.Lambda.RuntimeSupport
             if (str == null)
                 return null;
 
-            int n = str.Length;
-            var sb = new StringBuilder(n * 2);
-            for (int i = 0; i < n; i++)
-            {
-                char c = str[i];
-                switch (c)
-                {
-                    case '\n':
-                        sb.Append(@"\n");
-                        break;
+            string escapedString = JsonSerializer.Serialize(str);
 
-                    case '\r':
-                        sb.Append(@"\r");
-                        break;
-
-                    case '\t':
-                        sb.Append(@"\t");
-                        break;
-
-                    case '"':
-                        sb.Append(@"\""");
-                        break;
-
-                    case '\\':
-                        sb.Append(@"\\");
-                        break;
-
-                    case '\f':
-                        sb.Append(@"\f");
-                        break;
-
-                    case '\b':
-                        sb.Append(@"\b");
-                        break;
-
-                    case '\u0085': // Next Line
-                        sb.Append(@"\u0085");
-                        break;
-
-                    case '\u2028': // Line Separator
-                        sb.Append(@"\u2028");
-                        break;
-
-                    case '\u2029': // Paragraph Separator
-                        sb.Append(@"\u2029");
-                        break;
-
-                    default:
-                        if (c < ' ')
-                        {
-                            // Turn into a \uXXXX sequence
-                            sb.Append(@"\u");
-                            sb.Append(IntToHex((int)c));
-                        }
-                        else
-                        {
-                            sb.Append(c);
-                        }
-                        break;
-                }
-            }
-            return sb.ToString().Trim();
-        }
-
-        private static char[] IntToHex(int n)
-        {
-            int num;
-            char[] hex = new char[4];
-
-            for (int i = 0; i < 4; i++)
-            {
-                num = n % 16;
-
-                if (num < 10)
-                    hex[3 - i] = (char)('0' + num);
-                else
-                    hex[3 - i] = (char)('A' + (num - 10));
-
-                n >>= 4;
-            }
-            return hex;
+            return escapedString.Trim('"');
         }
     }
 }
