71374017fe5b14ed7e8083b329ca0d8c72b2981e
Norm Johanson <normj@amazon.com>
2023-11-14 15:56:36 -0800

    Add docs for LambdaSerializerInfo

--- Libraries/src/Amazon.Lambda.Annotations.SourceGenerator/Models/LambdaSerializerInfo.cs
+++ Libraries/src/Amazon.Lambda.Annotations.SourceGenerator/Models/LambdaSerializerInfo.cs
@@ -4,16 +4,30 @@ using System.Text;
 
 namespace Amazon.Lambda.Annotations.SourceGenerator.Models
 {
+    /// <summary>
+    /// Information for the registered ILambdaSerializer in the Lambda project.
+    /// </summary>
     public class LambdaSerializerInfo
     {
+        /// <summary>
+        /// Construct an instance of LambdaSerializerInfo
+        /// </summary>
+        /// <param name="serializerName"></param>
+        /// <param name="serializerJsonContextName"></param>
         public LambdaSerializerInfo(string serializerName, string serializerJsonContextName)
         {
             SerializerName = serializerName;
             SerializerJsonContextName = serializerJsonContextName;
         }
 
+        /// <summary>
+        /// The full name of the type registered as the ILambdaSerializer.
+        /// </summary>
         public string SerializerName { get; }
 
+        /// <summary>
+        /// The full name of the type used as the generic parameter of the SourceGeneratorLambdaJsonSerializer.
+        /// </summary>
         public string SerializerJsonContextName { get; }   
     }
 }
