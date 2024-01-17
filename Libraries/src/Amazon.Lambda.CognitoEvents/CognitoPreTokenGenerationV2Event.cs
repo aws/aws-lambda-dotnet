051b6cfa4028a0b031091f19d5aa51cbe15129da
Ernest Folch <ernest.folch@gmail.com>
2024-01-17 10:48:21 +0100

    feat: added contracts for cognito pre token generation v2_0

---
+++ Libraries/src/Amazon.Lambda.CognitoEvents/CognitoPreTokenGenerationV2Event.cs
@@ -0,0 +1,9 @@
+﻿namespace Amazon.Lambda.CognitoEvents
+{
+    /// <summary>
+    /// https://docs.aws.amazon.com/cognito/latest/developerguide/user-pool-lambda-pre-token-generation.html
+    /// </summary>
+    public class CognitoPreTokenGenerationV2Event : CognitoTriggerEvent<CognitoPreTokenGenerationV2Request, CognitoPreTokenGenerationV2Response>
+    {
+    }
+}
---
+++ Libraries/src/Amazon.Lambda.CognitoEvents/IdTokenGeneration.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Amazon.Lambda.CognitoEvents
+{
+    /// <summary>
+    /// https://docs.aws.amazon.com/cognito/latest/developerguide/user-pool-lambda-pre-token-generation.html
+    /// </summary>
+    [DataContract]
+    public class IdTokenGeneration
+    {
+        /// <summary>
+        /// A map of one or more key-value pairs of claims to add or override. For group related claims, use groupOverrideDetails instead.
+        /// </summary>
+        [DataMember(Name = "claimsToAddOrOverride")]
+#if NETCOREAPP3_1_OR_GREATER
+        [System.Text.Json.Serialization.JsonPropertyName("claimsToAddOrOverride")]
+# endif
+        public Dictionary<string, string> ClaimsToAddOrOverride { get; set; } = new Dictionary<string, string>();
+        
+        /// <summary>
+        /// A list that contains claims to be suppressed from the identity token.
+        /// </summary>
+        [DataMember(Name = "claimsToSuppress")]
+#if NETCOREAPP3_1_OR_GREATER
+        [System.Text.Json.Serialization.JsonPropertyName("claimsToSuppress")]
+# endif
+        public List<string> ClaimsToSuppress { get; set; } = new List<string>();
+    }
+}
