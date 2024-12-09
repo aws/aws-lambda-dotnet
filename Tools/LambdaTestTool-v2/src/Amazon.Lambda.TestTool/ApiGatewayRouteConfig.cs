2e94f8bfa833165c113860aeed294e320094572e
Garrett Beatty <gcbeatty@amazon.com>
2024-12-09 12:26:23 -0500

    Use extension method

---
+++ Tools/LambdaTestTool-v2/src/Amazon.Lambda.TestTool/ApiGatewayRouteConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace Amazon.Lambda.TestTool
+{
+    public class ApiGatewayRouteConfig
+    {
+        public string LambdaResourceName { get; set; }
+        public string Endpoint { get; set; }
+        public string HttpMethod { get; set; }
+        public string Path { get; set; }
+    }
+}
