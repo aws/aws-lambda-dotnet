6598bee200dfb108a643267442d1608f7c1d5075
Garrett Beatty <gcbeatty@amazon.com>
2025-01-21 14:44:52 -0500

    add response.clear back

--- Tools/LambdaTestTool-v2/src/Amazon.Lambda.TestTool/Extensions/ApiGatewayResponseExtensions.cs
+++ Tools/LambdaTestTool-v2/src/Amazon.Lambda.TestTool/Extensions/ApiGatewayResponseExtensions.cs
@@ -45,6 +45,7 @@ public static class ApiGatewayResponseExtensions
     public static async Task ToHttpResponseAsync(this APIGatewayHttpApiV2ProxyResponse apiResponse, HttpContext httpContext)
     {
         var response = httpContext.Response;
+        response.Clear();
 
         if (apiResponse.StatusCode == 0)
         {
