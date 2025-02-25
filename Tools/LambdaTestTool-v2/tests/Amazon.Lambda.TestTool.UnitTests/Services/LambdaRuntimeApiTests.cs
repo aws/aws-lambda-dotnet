95703681c1e8742e5b4dd46c0b954cefc4ab35b9
Garrett Beatty <gcbeatty@amazon.com>
2025-02-25 11:44:45 -0500

    add header

--- Tools/LambdaTestTool-v2/tests/Amazon.Lambda.TestTool.UnitTests/Services/LambdaRuntimeApiTests.cs
+++ Tools/LambdaTestTool-v2/tests/Amazon.Lambda.TestTool.UnitTests/Services/LambdaRuntimeApiTests.cs
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 using System.Text;
 using Amazon.Lambda.TestTool.Models;
 using Amazon.Lambda.TestTool.Services;
