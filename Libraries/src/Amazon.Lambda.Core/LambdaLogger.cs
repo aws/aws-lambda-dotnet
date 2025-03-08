b4723f9058f33e2b2d8293fa5089b7823820c3f0
Norm Johanson <normj@amazon.com>
2025-03-07 19:07:43 -0800

    Update global logger to support logging with level and arguments.

--- Libraries/src/Amazon.Lambda.Core/LambdaLogger.cs
+++ Libraries/src/Amazon.Lambda.Core/LambdaLogger.cs
@@ -1,4 +1,7 @@
-﻿using System;
+using System;
+using System.Reflection.Emit;
+using System.Runtime.Versioning;
+using System.Text;
 
 namespace Amazon.Lambda.Core
 {
@@ -9,8 +12,11 @@ namespace Amazon.Lambda.Core
     /// </summary>
     public static class LambdaLogger
     {
-        // Logging action, logs to Console by default
+        // The name of this field must not change or be readonly because Amazon.Runtime.Support will use reflection to replace the
+        // value with an Action that directs the logging into its logging system.
+#pragma warning disable IDE0044 // Add readonly modifier
         private static Action<string> _loggingAction = LogToConsole;
+#pragma warning restore IDE0044 // Add readonly modifier
 
         // Logs message to console
         private static void LogToConsole(string message)
@@ -29,5 +35,66 @@ namespace Amazon.Lambda.Core
         {
             _loggingAction(message);
         }
+
+#if NET6_0_OR_GREATER
+
+        // The name of this field must not change or be readonly because Amazon.Runtime.Support will use reflection to replace the
+        // value with an Action that directs the logging into its logging system.
+#pragma warning disable IDE0044 // Add readonly modifier
+        private static Action<string, string, object[]> _loggingWithLevelAction = LogWithLevelToConsole;
+#pragma warning restore IDE0044 // Add readonly modifier
+
+        // Logs message to console
+        private static void LogWithLevelToConsole(string level, string message, params object[] args)
+        {
+            // Formatting here is not important, it is used for debugging Amazon.Lambda.Core only.
+            // In a real scenario Amazon.Runtime.Support will change the value of _loggingWithLevelAction
+            // to an Action inside it's logging system to handle the real formatting.
+            var sb = new StringBuilder();
+            sb.Append(level).Append(": ").Append(message);
+            if (args?.Length > 0)
+            {
+                sb.Append(" Arguments:");
+                foreach(var arg in args)
+                {
+                    sb.Append(" \"");
+                    sb.Append(arg);
+                    sb.Append("\"");
+                }
+            }
+            Console.WriteLine(sb.ToString());
+        }
+
+        private const string ParameterizedPreviewMessage =
+            "This method has been mark as preview till the Lambda .NET Managed runtime has been updated with the backing implementation of this method. " +
+            "It is possible to use this method whilein preview if the Lambda function is deployed as an executable and uses the latest version of Amazon.Lambda.RuntimeSupport.";
+
+        /// <summary>
+        /// Logs a message to AWS CloudWatch Logs.
+        /// 
+        /// Logging will not be done:
+        ///  If the role provided to the function does not have sufficient permissions.
+        /// </summary>
+        /// <param name="level">The log level of the message</param>
+        /// <param name="message">Message to log. The message may have format arguments.</param>
+        /// <param name="args">Arguments to format the message with.</param>
+        [RequiresPreviewFeatures(ParameterizedPreviewMessage)]
+        public static void Log(string level, string message, params object[] args)
+        {
+            _loggingWithLevelAction(level, message, args);
+        }
+
+        /// <summary>
+        /// Logs a message to AWS CloudWatch Logs.
+        /// 
+        /// Logging will not be done:
+        ///  If the role provided to the function does not have sufficient permissions.
+        /// </summary>
+        /// <param name="level">The log level of the message</param>
+        /// <param name="message">Message to log. The message may have format arguments.</param>
+        /// <param name="args">Arguments to format the message with.</param>
+        [RequiresPreviewFeatures(ParameterizedPreviewMessage)]
+        public static void Log(LogLevel level, string message, params object[] args) => Log(level.ToString(), message, args);
+#endif
     }
 }
