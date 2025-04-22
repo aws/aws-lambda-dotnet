d5780f33e8bb0024de93f8f4288c24bce09e35e8
philip pittle <pittle@amazon.com>
2025-04-22 15:41:21 -0700

    Add additional test for asp.net workloads

--- Libraries/test/TestWebApp/HttpApiV2LambdaFunction.cs
+++ Libraries/test/TestWebApp/HttpApiV2LambdaFunction.cs
@@ -1,7 +1,16 @@
+using System.Collections.Generic;
+using System.Net.Http;
+using System;
 using Amazon.Lambda.AspNetCoreServer;
 namespace TestWebApp
 {
     public class HttpV2LambdaFunction : APIGatewayHttpApiV2ProxyFunction<Startup>
     {
+#if NET8_0_OR_GREATER
+        protected override IEnumerable<HttpRequestMessage> GetBeforeSnapshotRequests() =>
+        [
+            new HttpRequestMessage(HttpMethod.Get, "api/SnapStart")
+        ];
+#endif
     }
 }
