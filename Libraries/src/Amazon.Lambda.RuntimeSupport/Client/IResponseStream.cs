22d1469854413089fe883cf1dadc2b66f7adb860
Norm Johanson <normj@amazon.com>
2026-02-11 16:16:55 -0800

    Task 1

---
+++ Libraries/src/Amazon.Lambda.RuntimeSupport/Client/IResponseStream.cs
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Amazon.Lambda.RuntimeSupport
+{
+    /// <summary>
+    /// Interface for writing streaming responses in AWS Lambda functions.
+    /// Obtained by calling ResponseStreamFactory.CreateStream() within a handler.
+    /// </summary>
+    public interface IResponseStream : IDisposable
+    {
+        /// <summary>
+        /// Asynchronously writes a byte array to the response stream.
+        /// </summary>
+        /// <param name="buffer">The byte array to write.</param>
+        /// <param name="cancellationToken">Optional cancellation token.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        /// <exception cref="InvalidOperationException">Thrown if the stream is already completed or an error has been reported.</exception>
+        /// <exception cref="InvalidOperationException">Thrown if writing would exceed the 20 MiB limit.</exception>
+        Task WriteAsync(byte[] buffer, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Asynchronously writes a portion of a byte array to the response stream.
+        /// </summary>
+        /// <param name="buffer">The byte array containing data to write.</param>
+        /// <param name="offset">The zero-based byte offset in buffer at which to begin copying bytes.</param>
+        /// <param name="count">The number of bytes to write.</param>
+        /// <param name="cancellationToken">Optional cancellation token.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        /// <exception cref="InvalidOperationException">Thrown if the stream is already completed or an error has been reported.</exception>
+        /// <exception cref="InvalidOperationException">Thrown if writing would exceed the 20 MiB limit.</exception>
+        Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Asynchronously writes a memory buffer to the response stream.
+        /// </summary>
+        /// <param name="buffer">The memory buffer to write.</param>
+        /// <param name="cancellationToken">Optional cancellation token.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        /// <exception cref="InvalidOperationException">Thrown if the stream is already completed or an error has been reported.</exception>
+        /// <exception cref="InvalidOperationException">Thrown if writing would exceed the 20 MiB limit.</exception>
+        Task WriteAsync(ReadOnlyMemory<byte> buffer, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Reports an error that occurred during streaming.
+        /// This will send error information via HTTP trailing headers.
+        /// </summary>
+        /// <param name="exception">The exception to report.</param>
+        /// <param name="cancellationToken">Optional cancellation token.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        /// <exception cref="InvalidOperationException">Thrown if the stream is already completed or an error has already been reported.</exception>
+        Task ReportErrorAsync(Exception exception, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Gets the total number of bytes written to the stream so far.
+        /// </summary>
+        long BytesWritten { get; }
+
+        /// <summary>
+        /// Gets whether the stream has been completed.
+        /// </summary>
+        bool IsCompleted { get; }
+
+        /// <summary>
+        /// Gets whether an error has been reported.
+        /// </summary>
+        bool HasError { get; }
+    }
+}
---
+++ Libraries/src/Amazon.Lambda.RuntimeSupport/Client/StreamingConstants.cs
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+namespace Amazon.Lambda.RuntimeSupport
+{
+    /// <summary>
+    /// Constants used for Lambda response streaming.
+    /// </summary>
+    internal static class StreamingConstants
+    {
+        /// <summary>
+        /// Maximum response size for Lambda streaming responses: 20 MiB.
+        /// </summary>
+        public const long MaxResponseSize = 20 * 1024 * 1024;
+
+        /// <summary>
+        /// Header name for Lambda response mode.
+        /// </summary>
+        public const string ResponseModeHeader = "Lambda-Runtime-Function-Response-Mode";
+
+        /// <summary>
+        /// Value for streaming response mode.
+        /// </summary>
+        public const string StreamingResponseMode = "streaming";
+
+        /// <summary>
+        /// Trailer header name for error type.
+        /// </summary>
+        public const string ErrorTypeTrailer = "Lambda-Runtime-Function-Error-Type";
+
+        /// <summary>
+        /// Trailer header name for error body.
+        /// </summary>
+        public const string ErrorBodyTrailer = "Lambda-Runtime-Function-Error-Body";
+    }
+}
