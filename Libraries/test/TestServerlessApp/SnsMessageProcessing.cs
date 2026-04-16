fae3446a0b1335a56428c975e9d7dbc76662599d
Garrett Beatty <gcbeatty@amazon.com>
2026-04-16 10:57:04 -0400

    add header

--- Libraries/test/TestServerlessApp/SnsMessageProcessing.cs
+++ Libraries/test/TestServerlessApp/SnsMessageProcessing.cs
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 using Amazon.Lambda.Annotations;
 using Amazon.Lambda.Annotations.SNS;
 using Amazon.Lambda.Core;
