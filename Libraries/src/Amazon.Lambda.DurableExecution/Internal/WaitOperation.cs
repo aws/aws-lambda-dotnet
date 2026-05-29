00558a1dbc9064940cb53a17e248e82e71da346f
Garrett Beatty <gcbeatty@amazon.com>
2026-05-29 14:22:27 -0400

    license

--- Libraries/src/Amazon.Lambda.DurableExecution/Internal/WaitOperation.cs
+++ Libraries/src/Amazon.Lambda.DurableExecution/Internal/WaitOperation.cs
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 using Amazon.Lambda;
 using SdkOperationUpdate = Amazon.Lambda.Model.OperationUpdate;
 using SdkWaitOptions = Amazon.Lambda.Model.WaitOptions;
--- Libraries/src/Amazon.Lambda.DurableExecution/UpperSnakeCaseEnumConverter.cs
+++ Libraries/src/Amazon.Lambda.DurableExecution/UpperSnakeCaseEnumConverter.cs
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
